f07d84ac1e737f29c337617aca6201f6d1703816
shroukshams <shroukshams237@gmail.com>
2026-05-09 21:34:00 +0300

    Add project files.

---
+++ G-NET-9-EF04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET_9_EF04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
